4ecc76c0ede2ffc6f55aa787801942fa1bf7c7ff
Juan Cruz Poli <juancruzpoli@MacBook-Pro-de-wolox.local>
2020-10-14 13:55:30 -0300

    Empty lines added - Karate-config file updated and environment file refactored

--- KarateTraining/src/test/java/karate-config.js
+++ KarateTraining/src/test/java/karate-config.js
@@ -1,26 +1,16 @@
 function fn() {
   var env = karate.env; // get system property 'karate.env'
-  var jsonConfig = read('classpath:src/main/resources/qa.json');
   karate.log('karate.env system property was:', env);
-  if (!env) {
+  if (!env){
     env = 'qa';
-    var config = {
-        env: env,
-        baseUrl: jsonConfig.baseQaUrl
-      }
-  }
+    }
 
-  if (env == 'dev') {
+  var jsonConfig = read('classpath:src/main/resources/environments.json');
+
   var config = {
-          env: env,
-          baseUrl: jsonConfig.baseDevUrl
-        }
-      } else if (env == 'stg') {
-      var config = {
-              env: env,
-              baseUrl: jsonConfig.baseStgUrl
-            }
-      }
-
+        env: env,
+        baseUrl: jsonConfig[env].baseUrl
+    }
   return config;
+
 }
